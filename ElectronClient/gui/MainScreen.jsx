979e8ec57aeff5f0d1baac4623a546541e422507
Giacomo Rossetto <jackyman_cs4@live.it>
2020-03-26 13:22:51 +0100

    Add indentation for deeper notebooks

--- ElectronClient/gui/MainScreen.jsx
+++ ElectronClient/gui/MainScreen.jsx
@@ -209,11 +209,18 @@ class MainScreenComponent extends React.Component {
 				},
 			});
 		} else if (command.name === 'moveToFolder') {
-			const folderItems = this.props.folders;
-			const startFolders = folderItems
-				.map(a => {
-					return { key: a.id, value: a.id, label: a.title, indentDepth: a.parent_id == '' ? 0 : 1 };
-				});
+
+			const startFolders = [];
+			const maxDepth = 20;
+
+			const addOptions = (folders, depth) => {
+				for (let i = 0; i < folders.length; i++) {
+					const folder = folders[i];
+					startFolders.push({ key: folder.id, value: folder.id, label: folder.title, indentDepth: depth });
+					if (folder.children) addOptions(folder.children, (depth + 1) < maxDepth ? depth + 1 : maxDepth);
+				}
+			};
+			addOptions(await Folder.allAsTree(), 0);
 
 			this.setState({
 				promptOptions: {
